ae4d7691889ae7928d339460b8719139cf6c74d5
Liz Izatt <liz@berkeley.edu>
2018-10-23 21:48:21 -0700

    Added glm

--- build/libraries/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Liz/TicTacToe/libraries/glm
-# Build directory: C:/Users/Liz/TicTacToe/build/libraries/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- build/libraries/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Liz/TicTacToe/libraries/glm/glm
-# Build directory: C:/Users/Liz/TicTacToe/build/libraries/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
